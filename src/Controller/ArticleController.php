c42698ff5a7c843f80c08f19832efa752bd0971b
OLG-MAN <mandrik89@gmail.com>
2020-03-02 23:37:39 +0200

    articles

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,11 +3,11 @@
 
 namespace App\Controller;
 
-
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -19,9 +19,20 @@ class ArticleController
 
     /**
      * @Route("/news/{slug}")
+     * @param $slug
+     * @return Response
      */
     public function show($slug)
     {
-        return new Response(sprintf('New article page: %s', $slug));
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'yahoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bandsman.com',
+        ];
+
+        return $this->render('news/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments,
+        ]);
     }
 }
