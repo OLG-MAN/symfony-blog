3a5f35e34109f625acae6609872aa3d1ef0f8474
OLG-MAN <mandrik89@gmail.com>
2020-03-03 18:41:15 +0200

    hearts on cosmic news article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -32,7 +32,17 @@ class ArticleController extends AbstractController
 
         return $this->render('news/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
+            'slug' => $slug,
             'comments' => $comments,
         ]);
+    }
+
+    /**
+     * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+     */
+    public function toggleArticleHeart($slug)
+    {
+        // TODO - actually heart/unheart the article!
+        return $this->json(['hearts' => rand(5, 100)]);
     }
 }
